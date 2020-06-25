9351f79e20bf424e196209bb2d23eefa33155dfb
finngreiter <finn.greiter@gmail.com>
2020-06-24 20:39:24 -0700

    made better install script

--- shiftinstall.sh
+++ shiftinstall.sh
@@ -2,3 +2,9 @@ SHIFT_LOCATION=/Users/$(whoami)/.shiftfiles
 mkdir -p $SHIFT_LOCATION
 mkdir -p $SHIFT_LOCATION/bin
 mkdir -p $SHIFT_LOCATION/files
+curl -fsSL https://raw.githubusercontent.com/finngreiter/shift/master/packages/shift -o $SHIFT_LOCATION/bin/shift
+clear
+echo '### SHIFT ###'
+echo 'install complete!'
+echo '### SHIFT ###'
+echo add $SHIFT_LOCATION/bin to path
